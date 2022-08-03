7c5cc6aa2b284cccdf14dcfe093f1ae3c6b96e09
25081997pc <chavanpooja2508@gmail.com>
2022-08-03 19:22:56 +0530

    Adding userRegister

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace UserRegistration_Program
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        //In this Program validating user input with the regular expression.
+        public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the User Registration Program");
+            UserRegister userRegister = new UserRegister();
+            Console.WriteLine("Enter the First Name: ");
+            string firstname = Console.ReadLine();
+
+            if (userRegister.validateFirstName(firstname))
+            {
+                Console.WriteLine("Pattern Match");
+            }
+            else
+            {
+                Console.WriteLine("Pattern not Match");
+            }
         }
     }
 }
