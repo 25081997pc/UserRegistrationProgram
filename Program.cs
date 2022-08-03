41083037e2ce73ac0f385a57c8ab2df111af7c76
25081997pc <chavanpooja2508@gmail.com>
2022-08-03 19:41:41 +0530

    Adding userRegester

--- Program.cs
+++ Program.cs
@@ -7,16 +7,17 @@
         {
             Console.WriteLine("Welcome to the User Registration Program");
             UserRegister userRegister = new UserRegister();
-            Console.WriteLine("Enter the First Name: ");
-            string firstname = Console.ReadLine();
+            //Validating Last Name
+            Console.WriteLine("Enter the Last Name: ");
+            string lastname = Console.ReadLine();
 
-            if (userRegister.validateFirstName(firstname))
+            if (userRegister.validateLastName(lastname))
             {
-                Console.WriteLine("Pattern Match");
+                Console.WriteLine("LastName is valid");
             }
             else
             {
-                Console.WriteLine("Pattern not Match");
+                Console.WriteLine("LastName is not valid");
             }
         }
     }
