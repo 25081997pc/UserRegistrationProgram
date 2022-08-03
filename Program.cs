2765b4b83eb7cdc76e64650005c7d65f69d247a6
25081997pc <chavanpooja2508@gmail.com>
2022-08-03 18:38:17 +0530

    Welcom message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRegistration_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the User Registration Program");
+        }
+    }
+}
