c712e1c39760b77d7d5fcc538824761cbb3983d9
25081997pc <chavanpooja2508@gmail.com>
2022-08-03 19:24:00 +0530

    Validating FirstName

---
+++ UserRegister.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration_Program
+{
+    public class UserRegister
+    {
+        public string regex_FirstName = "^[A-Z][a-z]{2,}$";
+
+        public bool validateFirstName(string firstname)
+        {
+            return Regex.IsMatch(firstname, regex_FirstName);
+        }
+    }
+}
