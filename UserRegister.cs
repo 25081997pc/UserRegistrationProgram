ec41da3daa0538be8f842151b7436147dce11c4c
25081997pc <chavanpooja2508@gmail.com>
2022-08-03 22:50:04 +0530

    Validating Password it should have 1 number

--- UserRegister.cs
+++ UserRegister.cs
@@ -14,7 +14,7 @@ namespace UserRegistration_Program
         public string regex_LastName = "^[A-Z][a-z]{2,}$";
         public string regex_Email = "^[a-zA-Z0-9]+([.][a-zA-Z0-9]+)*[@][a-zA-Z0-9]{2,}[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,})?$";
         public string regex_PhoneNumber = "^[+]?[9][1][' ']?[0-9]{10}$";
-        public string regex_Password = "^[A-Z][a-z]{7,}$";
+        public string regex_Password = "^[A-Z][a-zA-Z0-9]{7,}$";
         //Method to check the First Name
         public bool validateFirstName(string firstname)
         {
