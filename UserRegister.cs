dd13d58629c643b98cc806a6780c9f9efe54c5f0
25081997pc <chavanpooja2508@gmail.com>
2022-08-03 19:42:33 +0530

    Validating LastName

--- UserRegister.cs
+++ UserRegister.cs
@@ -10,10 +10,14 @@ namespace UserRegistration_Program
     public class UserRegister
     {
         public string regex_FirstName = "^[A-Z][a-z]{2,}$";
-
+        public string regex_LastName = "^[A-Z][a-z]{2,}$";
         public bool validateFirstName(string firstname)
         {
             return Regex.IsMatch(firstname, regex_FirstName);
+        }
+        public bool validateLastName(string lastname)
+        {
+            return Regex.IsMatch(lastname, regex_LastName);
         }
     }
 }
